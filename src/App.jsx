9e9afc928a0cf98720ada25ec8ea762f02accb70
vignesh220402 <skvigneshsivakumar@gmail.com>
2025-02-11 07:19:15 +0530

    commit final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/home"
 import User from "./pages/User"
 import About from "./pages/About"
@@ -6,17 +6,18 @@ import Contact from "./pages/Contact"
 import BlogSection from "./pages/BlogSection"
 
 function App() {
-  
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/services" element={<User/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/blogsection" element={<BlogSection/>}/>
-    </Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<User />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blogsection" element={<BlogSection />} />
+      </Routes>
+    </Router>
+
   )
 }
 
